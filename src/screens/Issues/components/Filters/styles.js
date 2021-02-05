4b1da93af8e9be1057c73b0529e8b5fcefdeacc5
Alexander Baltsevich <baltsevicha@gmail.com>
2021-02-05 17:48:29 +0300

    updated styles for platform specific cases

--- src/screens/Issues/components/Filters/styles.js
+++ src/screens/Issues/components/Filters/styles.js
@@ -1,7 +1,17 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Platform } from 'react-native';
 
 export default StyleSheet.create({
   text: {
     color: '#ffffff',
+    ...Platform.select({
+      android: {
+        paddingRight: 18,
+        paddingLeft: 18,
+        fontSize: 18,
+      },
+      ios: {
+        fontWeight: 'bold',
+      },
+    }),
   },
 });
